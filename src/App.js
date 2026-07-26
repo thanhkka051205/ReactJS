b46b10481d1ae43dea8d4790ff2888dbab3a0c80
Thanh Kka <thuyminet0@gmail.com>
2026-07-26 13:38:42 +0700

    git commit -m "Hoc React"

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MyComponent from "./components/MyComponent";
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      Thành Kka
-      <MyComponent></MyComponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hello world with React.js (Hoi Dan IT)
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Thành Kka
+        <MyComponent></MyComponent>
+      </div>
+    );
+  }
 }
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello world with React.js (Hoi Dan IT)
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 export default App;
