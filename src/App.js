310c2d88b943158cb0513b48277c7d56b630635d
Thanh Kka <thuyminet0@gmail.com>
2026-07-26 12:37:33 +0700

    git commit -m 'first commit'

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+import MyComponent from "./components/MyComponent";
+
+function App() {
+  return (
+    <div>
+      Thành Kka
+      <MyComponent></MyComponent>
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Hello world with React.js (Hoi Dan IT)
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  //JSX
+  render() {
+    return <div>my first component</div>;
+  }
+}
+
+export default MyComponent;
