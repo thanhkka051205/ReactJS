44e3d0d5e5156ee3a113bbb2f88818f19a39ccd6
Thanh Kka <thuyminet0@gmail.com>
2026-07-26 15:18:55 +0700

    git commit -m "Lam quen voi props va hien thi danh sach Array"

---
+++ src/components/DisplayInfor.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class DisplayInfor extends React.Component {
+  render() {
+    const { listUsers } = this.props;
+    return (
+      <div>
+        {listUsers.map((user) => {
+          return (
+            <div key={user.id}>
+              <div>My name is {user.name}</div>
+              <div>My age is {user.age}</div>
+              <hr />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default DisplayInfor;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,10 +1,25 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "Thanh Kka", age: "21" },
+      { id: 2, name: "Thanh Ma", age: "20" },
+      { id: 3, name: "Thanh Ok", age: "21" },
+    ],
+  };
+
   //JSX
   render() {
-    return <UserInfor />;
+    return (
+      <div>
+        <UserInfor />
+        <br />
+        <DisplayInfor listUsers={this.state.listUsers} />
+      </div>
+    );
   }
 }
 
